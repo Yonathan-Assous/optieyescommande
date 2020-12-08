379724ac04a946529c30fd4822b12ffb2e8672cf
Yonathan Assous <yonathanassous@gmail.com>
2020-12-08 10:28:48 +0100

    log

---
+++ application/logs/log-2020-12-07.php
@@ -0,0 +1,5 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2020-12-07 08:40:01 --> Query error: Field 'has_order' doesn't have a default value - INSERT INTO user_sessions (user_id, day, login_time, login_count) VALUES ('46', 1607295600, 1607326801, 1) ON DUPLICATE KEY UPDATE login_time = 1607326801, login_count = login_count +1 
+ERROR - 2020-12-07 13:29:13 --> Query error: Field 'has_order' doesn't have a default value - INSERT INTO user_sessions (user_id, day, login_time, login_count) VALUES ('46', 1607295600, 1607344153, 1) ON DUPLICATE KEY UPDATE login_time = 1607344153, login_count = login_count +1 
+ERROR - 2020-12-07 14:17:38 --> Query error: Field 'has_order' doesn't have a default value - INSERT INTO user_sessions (user_id, day, login_time, login_count) VALUES ('46', 1607295600, 1607347058, 1) ON DUPLICATE KEY UPDATE login_time = 1607347058, login_count = login_count +1 

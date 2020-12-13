f6555cfec74a5cacb04b30d224e88db6c64b46c4
= <yonathanassous@gmail.com>
2020-12-13 14:58:57 +0200

    gitignore

--- application/config/database.php
+++ application/config/database.php
@@ -50,9 +50,9 @@ $active_record = TRUE;
 
 
 $db['new']['hostname'] = 'localhost';
-$db['new']['username'] = 'root';
-$db['new']['password'] = '';
-$db['new']['database'] = 'optieyes';
+$db['new']['username'] = 'optieyesproxi548';
+$db['new']['password'] = 'P6O1L238z9i6wYq';
+$db['new']['database'] = 'optieyesproxi5489ytg85';
 $db['new']['dbdriver'] = 'mysqli';
 $db['new']['dbprefix'] = '';
 $db['new']['pconnect'] = FALSE;

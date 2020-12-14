89fee296a40d2cee5dbae85c5c0a71d5cf498489
= <yonathanassous@gmail.com>
2020-12-14 10:12:13 +0200

    INNER JOIN email commande

---
+++ application/logs/log-2020-12-14.php
@@ -0,0 +1,50 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2020-12-14 07:46:34 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ') AND date_commande > "2020-12-14 00:00:00" AND date_commande < "2020-12-14 23:5' at line 1 - SELECT tarif_packaging AS total, MAX(date_commande) FROM commande WHERE id_users IN() AND date_commande > "2020-12-14 00:00:00" AND date_commande < "2020-12-14 23:59:59" GROUP BY id_users ORDER BY date_commande DESC
+ERROR - 2020-12-14 07:47:42 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:42 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:43 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:43 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:43 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:43 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:43 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:43 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:43 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:43 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:43 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:43 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:43 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:43 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:43 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:43 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:44 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:44 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:44 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:44 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:44 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:44 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:44 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:44 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:44 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:44 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:44 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:44 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:44 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:44 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:44 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:44 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:44 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:44 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:45 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:45 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:45 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:45 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:45 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:45 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:45 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:47:45 --> Severity: Warning  --> session_destroy(): Session object destruction failed C:\laragon_full\www\optieyescommande\application\libraries\Session.php 74
+ERROR - 2020-12-14 07:51:55 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'commande FROM commande WHERE origine_commande = -1000' at line 1 - SELECT * commande FROM commande WHERE origine_commande = -1000
+ERROR - 2020-12-14 07:52:16 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'commande FROM commande WHERE origine_commande = -1000' at line 1 - SELECT * commande FROM commande WHERE origine_commande = -1000
+ERROR - 2020-12-14 07:52:22 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'commande FROM commande WHERE origine_commande = -1000' at line 1 - SELECT * commande FROM commande WHERE origine_commande = -1000
+ERROR - 2020-12-14 07:54:49 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'commande FROM commande WHERE id_commande = -1000' at line 1 - SELECT * commande FROM commande WHERE id_commande = -1000
+ERROR - 2020-12-14 07:55:43 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'commande FROM commande WHERE id_commande = 1' at line 1 - SELECT * commande FROM commande WHERE id_commande = 1

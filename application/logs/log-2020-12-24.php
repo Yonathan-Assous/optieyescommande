0cc21b18a1058bf5d3d5365270b8a4f13eab3332
Yonathan Assous <yonathanassous@gmail.com>
2020-12-24 10:21:29 +0100

    config

---
+++ application/logs/log-2020-12-24.php
@@ -0,0 +1,52 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined index: user_info /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/controllers/index.php 3831
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Trying to get property of non-object /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/controllers/index.php 3831
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: title /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/header.php 17
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: page /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/header.php 76
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: page /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 7
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: page /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 7
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: page /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 9
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: page /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 10
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: page /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 12
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: page /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 13
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: page /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 15
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: page /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 16
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: page /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 22
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: page /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 22
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: page /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 24
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: page /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 25
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: page /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 27
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: page /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 28
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: page /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 30
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: page /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 31
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: page /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 36
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: page /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 37
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: page /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 40
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: page /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 41
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: page /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 52
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: page /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 53
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: user_info /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 67
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Trying to get property of non-object /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/menu.php 67
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: user_info /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/dashboard.php 46
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Trying to get property of non-object /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/dashboard.php 46
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: user_info /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/dashboard.php 52
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Trying to get property of non-object /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/dashboard.php 52
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: user_info /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/dashboard.php 65
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Trying to get property of non-object /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/dashboard.php 65
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: user_info /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/dashboard.php 108
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Trying to get property of non-object /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/dashboard.php 108
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: user_info /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/dashboard.php 124
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Trying to get property of non-object /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/dashboard.php 124
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: user_info /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/dashboard.php 132
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Trying to get property of non-object /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/dashboard.php 132
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: type_commande /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/dashboard.php 1964
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: type_commande /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/dashboard.php 2025
+ERROR - 2020-12-24 01:00:17 --> Severity: Notice  --> Undefined variable: region /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/views/dashboard.php 2123
+ERROR - 2020-12-24 06:25:29 --> Query error: Field 'has_order' doesn't have a default value - INSERT INTO user_sessions (user_id, day, login_time, login_count) VALUES ('46', 1608764400, 1608787529, 1) ON DUPLICATE KEY UPDATE login_time = 1608787529, login_count = login_count +1 
+ERROR - 2020-12-24 07:52:39 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ') AND date_commande > "2020-12-24 00:00:00" AND date_commande < "2020-12-24 2...' at line 1 - SELECT tarif_packaging AS total, MAX(date_commande) FROM commande WHERE id_users IN() AND date_commande > "2020-12-24 00:00:00" AND date_commande < "2020-12-24 23:59:59" GROUP BY id_users ORDER BY date_commande DESC
+ERROR - 2020-12-24 07:58:33 --> Severity: Notice  --> Undefined property: stdClass::$correction_droit /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/controllers/admin.php 6624
+ERROR - 2020-12-24 07:58:33 --> Severity: Notice  --> Trying to get property of non-object /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/controllers/admin.php 6624
+ERROR - 2020-12-24 07:58:33 --> Severity: Notice  --> Undefined property: stdClass::$correction_droit /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/controllers/admin.php 6634
+ERROR - 2020-12-24 07:58:33 --> Severity: Notice  --> Trying to get property of non-object /var/www/vhosts/optieyescommande.com/httpdocs_dev/application/controllers/admin.php 6634
+ERROR - 2020-12-24 09:19:12 --> Query error: Field 'has_order' doesn't have a default value - INSERT INTO user_sessions (user_id, day, login_time, login_count) VALUES ('46', 1608764400, 1608797952, 1) ON DUPLICATE KEY UPDATE login_time = 1608797952, login_count = login_count +1 

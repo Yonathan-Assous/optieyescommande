4ce191f5ccd76cbaec138f81f337aa91edf11ded
= <yonathanassous@gmail.com>
2020-12-27 12:01:46 +0200

    m_type_verre_solaire

---
+++ application/models/m_type_verre_solaire.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class m_type_verre_solaire extends CI_Model
+{
+    var $table = 'type_verre_solaire';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}

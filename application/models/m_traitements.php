1359808fc971f4fcf1191fca39dbcf1811b42708
= <yonathanassous@gmail.com>
2020-12-28 11:46:32 +0200

    ajout des prix des traitements

--- application/models/m_traitements.php
+++ application/models/m_traitements.php
@@ -38,7 +38,8 @@ class m_traitements extends CI_Model
         foreach ($traitements as $traitement) {
             $traitementArray[$traitement->id] = $traitement->name;
         }
-
+        $count = 0;
+        $null = false;
         $indices = ['1,53' => 2, '1,56' => 3, '1,5' => 1,  '1,67' => 5, '1,6' => 4, '1,74' => 6, '1,591' => 8];
         foreach ($lenses as $lens) {
             foreach ($indices as $indice => $indice_id) {
@@ -56,7 +57,8 @@ class m_traitements extends CI_Model
             }
             if (stripos($lens->trad_fr, 't-one') !== FALSE) {
                 if ($idTypeVerreSolaire == NULL || $typeVerreSolaire->name == 'BlueCoat Defender') {
-                    $prices = ['Durci' => 0,
+                    $prices = [
+                        'Durci' => 0,
                         'HMC' => 7,
                         'HMC Face Interne' => 7,
                         'Satin' => 12,
@@ -69,11 +71,15 @@ class m_traitements extends CI_Model
                         'Satin Max' => 22,
                         'Satin Max Face Interne' => 22,
                         'Satin Drive Or' => 22,
-                        'Miroir' => 14
+                        'Miroir Argent HMC face interne' => 14,
+                        'Miroir Doré HMC face interne' => 14,
+                        'Miroir Bleu HMC face interne' => 14,
+                        'Miroir Flash HMC face interne' => 14,
                         ];
                 }
                 else {
-                    $prices = ['Durci' => 0,
+                    $prices = [
+                        'Durci' => 0,
                         'HMC' => 4,
                         'HMC Face Interne' => 4,
                         'Satin' => 8,
@@ -86,7 +92,10 @@ class m_traitements extends CI_Model
                         'Satin Max' => 18,
                         'Satin Max Face Interne' => 18,
                         'Satin Drive Or' => 18,
-                        'Miroir' => 14
+                        'Miroir Argent HMC face interne' => 14,
+                        'Miroir Doré HMC face interne' => 14,
+                        'Miroir Bleu HMC face interne' => 14,
+                        'Miroir Flash HMC face interne' => 14,
                     ];
                 }
             }
@@ -105,7 +114,10 @@ class m_traitements extends CI_Model
                         'Satin Max' => 21,
                         'Satin Max Face Interne' => 21,
                         'Satin Drive Or' => 21,
-                        'Miroir' => 14
+                        'Miroir Argent HMC face interne' => 14,
+                        'Miroir Doré HMC face interne' => 14,
+                        'Miroir Bleu HMC face interne' => 14,
+                        'Miroir Flash HMC face interne' => 14,
                     ];
                 }
                 else {
@@ -122,7 +134,10 @@ class m_traitements extends CI_Model
                         'Satin Max' => 17,
                         'Satin Max Face Interne' => 17,
                         'Satin Drive Or' => 17,
-                        'Miroir' => 14
+                        'Miroir Argent HMC face interne' => 14,
+                        'Miroir Doré HMC face interne' => 14,
+                        'Miroir Bleu HMC face interne' => 14,
+                        'Miroir Flash HMC face interne' => 14,
                     ];
                 }
             }
@@ -146,8 +161,10 @@ class m_traitements extends CI_Model
                     'Satin Max' => 18,
                     'Satin Max Face Interne' => 18,
                     'Satin Drive Or' => 18,
-                    'Miroir' => 14
-                ];
+                    'Miroir Argent HMC face interne' => 14,
+                    'Miroir Doré HMC face interne' => 14,
+                    'Miroir Bleu HMC face interne' => 14,
+                    'Miroir Flash HMC face interne' => 14,                ];
             }
             else if (stripos($lens->trad_fr, 'Freestyle') !== FALSE
                 || stripos($lens->trad_fr, 'EyeFatigue') !== FALSE
@@ -166,7 +183,10 @@ class m_traitements extends CI_Model
                         'Satin Max' => 17,
                         'Satin Max Face Interne' => 17,
                         'Satin Drive Or' => 17,
-                        'Miroir' => 14
+                        'Miroir Argent HMC face interne' => 14,
+                        'Miroir Doré HMC face interne' => 14,
+                        'Miroir Bleu HMC face interne' => 14,
+                        'Miroir Flash HMC face interne' => 14,
                     ];
                 } else {
                     $prices = ['Durci' => 0,
@@ -182,17 +202,33 @@ class m_traitements extends CI_Model
                         'Satin Max' => 17,
                         'Satin Max Face Interne' => 17,
                         'Satin Drive Or' => 17,
-                        'Miroir' => 14
+                        'Miroir Argent HMC face interne' => 14,
+                        'Miroir Doré HMC face interne' => 14,
+                        'Miroir Bleu HMC face interne' => 14,
+                        'Miroir Flash HMC face interne' => 14,
                     ];
                 }
             }
+//            if ($idTypeVerreSolaire == NULL) {
+//                var_dump($lens->trad_fr);die;
+//            }
+
             foreach ($traitementArray as $traitementId => $traitementName) {
                 $price = $prices[$traitementName];
-                $sql = "INSERT INTO traitement_prix (id_traitement, id_lenses, id_indice_verre, id_type_verre_solaire, price) VALUES ('".$traitementId."','".$lens->id."','".$indiceId."','".$idTypeVerreSolaire."','".$price."')";
+                if ($idTypeVerreSolaire) {
+                    $sql = "INSERT INTO traitement_prix (id_traitement, id_lenses, id_indice_verre, id_type_verre_solaire, price) VALUES ('".$traitementId."','".$lens->id."','".$indiceId."','".$idTypeVerreSolaire."','".$price."')";
+                }
+                else {
+                    $sql = "INSERT INTO traitement_prix (id_traitement, id_lenses, id_indice_verre, id_type_verre_solaire, price) VALUES ('"
+                           .$traitementId."','".$lens->id."','".$indiceId."',NULL,'".$price."')";
+                }
+                if ($idTypeVerreSolaire == NULL) {
+                    $null = true;
+                }
                 //var_dump($sql);die;
                 $this->db->query($sql);
             }
         }
-//        $this->db->query("UPDATE `verres` SET `prix_traitement` = 0 WHERE `libelle_verre` LIKE '%Durci%' AND `libelle_verre` NOT LIKE '%freestyle%' AND `libelle_verre` NOT LIKE '%EyeFatigue%'");
+        $this->db->query("UPDATE `verres` SET `prix_traitement` = 0 WHERE `libelle_verre` LIKE '%Durci%' AND `libelle_verre` NOT LIKE '%freestyle%' AND `libelle_verre` NOT LIKE '%EyeFatigue%'");
     }
 }

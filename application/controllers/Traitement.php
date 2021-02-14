b96cf79a6c898b9544ef7406b5360836fc35180c
Yonathan Assous <yonathanassous@gmail.com>
2021-02-14 12:22:24 +0200

    test

--- application/controllers/Traitement.php
+++ application/controllers/Traitement.php
@@ -1,50 +1,13 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Traitement extends MY_Controller
-{
-    public function index()
-    {
-        var_dump('Hello World!');
+class traitement extends MY_Controller {
+
+    public function __construct() {
+        parent::__construct();
     }
 
-    public function setTraitementsInDatabase() {
-//        var_dump('die');die;
-        $this->m_traitement->insertPrixTraitement();
+    public function index(){
+        var_dump('dsadsadsa');
     }
 
-    public function getTraitementPriceList()
-    {
-        $data =
-            $this->input->post();
-
-        $tab =
-            $this->m_traitement->getTraitementPriceList($data['user_id']);
-        echo json_encode($tab);
-    }
-
-    public function setPriceTraitement() {
-        $data =
-            $this->input->post();
-
-        $result =
-            $this->m_traitement->setPriceTraitement(
-                $data['new_price'],
-                $data['code_verre'],
-                $data['name_verre'],
-                $data['traitement_id'],
-                $data['user_id']
-            );
-
-        echo $result;
-    }
-
-    public function desactivePriceTraitement() {
-        $data = $this->input->post();
-        $result = $this->m_traitement->desactivePriceTraitement(
-            $data['lens_code'],
-            $data['traitement_id'],
-            $data['user_id']
-        );
-        echo $result;
-    }
 }

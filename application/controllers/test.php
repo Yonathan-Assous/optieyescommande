21f1e435c1b6b5dff5594ad5bcec602e9d4b9912
Yonathan Assous <yonathanassous@gmail.com>
2021-02-14 12:27:07 +0200

    test

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -2,12 +2,50 @@
 
 class test extends MY_Controller {
 
-    public function __construct() {
-        parent::__construct();
+    public function index()
+    {
+        var_dump('Hello World!');
     }
 
-    public function index(){
-        $this->load->view('catalogue');
+    public function setTraitementsInDatabase() {
+//        var_dump('die');die;
+        $this->m_traitement->insertPrixTraitement();
+    }
+
+    public function getTraitementPriceList()
+    {
+        $data =
+            $this->input->post();
+
+        $tab =
+            $this->m_traitement->getTraitementPriceList($data['user_id']);
+        echo json_encode($tab);
+    }
+
+    public function setPriceTraitement() {
+        $data =
+            $this->input->post();
+
+        $result =
+            $this->m_traitement->setPriceTraitement(
+                $data['new_price'],
+                $data['code_verre'],
+                $data['name_verre'],
+                $data['traitement_id'],
+                $data['user_id']
+            );
+
+        echo $result;
+    }
+
+    public function desactivePriceTraitement() {
+        $data = $this->input->post();
+        $result = $this->m_traitement->desactivePriceTraitement(
+            $data['lens_code'],
+            $data['traitement_id'],
+            $data['user_id']
+        );
+        echo $result;
     }
 
 }

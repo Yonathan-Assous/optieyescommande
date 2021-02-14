087d08cea3748069a0fad2504054bfb337daae6e
Yonathan Assous <yonathanassous@gmail.com>
2021-02-14 12:37:05 +0200

    test

--- application/controllers/traitement.php
+++ application/controllers/traitement.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class test extends MY_Controller {
+class traitement extends MY_Controller {
 
     public function index()
     {

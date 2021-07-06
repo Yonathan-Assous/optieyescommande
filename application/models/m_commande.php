3acd2183dd8c9518ed38bbf4ef734124bd3327e3
Yonathan Assous <yonathanassous@gmail.com>
2021-07-06 17:01:52 +0300

    comparaison de verre stock

--- application/models/m_commande.php
+++ application/models/m_commande.php
@@ -617,7 +617,7 @@ class m_commande extends CI_Model {
                                    LEFT JOIN ".$table_commentaire." cc ON cc.id_commande = c.id_commande
                                    LEFT JOIN intitule_bl ib ON c.id_commande = ib.id_commande
                                    WHERE c.id_commande=".$id_commande." ".$sql_add."
-                                   AND v.display = 'X'
+                                   AND (v.display = 'X' OR v_stock.id_verre IS NOT NULL)
                                    ORDER BY date_commande DESC";
         $query = $this->db->query($sql);
 

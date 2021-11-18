620bde925ba9ae34b268a203b2fc4aa288e892ce
Yonathan Assous <yonathanassous@gmail.com>
2021-11-18 12:00:09 +0200

    test

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -3990,7 +3990,7 @@ class admin
                          $key
                 => $commande)
                 {
-                    //print_r($commande);die;
+                    print_r($commande);
                     $typeDeVerre = $this->m_lenses->getLensesByCode($commande->id_verre);
                     if (!$typeDeVerre) {
                         $typeDeVerre = $this->m_verres_stock->getByIdVerre($commande->id_verre);
@@ -4259,6 +4259,7 @@ class admin
 
 //                    var_dump($data);die;
                 }
+                die;
             }
 //            var_dump($data);die;
             die(json_encode($data));

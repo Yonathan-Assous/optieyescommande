ce6b1ced12d2b8398fc80b052a4198a1e1f2ba3b
Yonathan Assous <yonathanassous@gmail.com>
2022-03-15 10:08:06 +0200

    backup.sh

--- backup.sh
+++ backup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-mysqldump -u optieyesproxi548 -p P6O1L238z9i6wYq optieyesproxi5489ytg85 > backup.sql;
+mysqldump -uoptieyesproxi548 -pP6O1L238z9i6wYq optieyesproxi5489ytg85 > backup.sql;
+gupload backup.sql
+echo "Finished"
+# yoni, push

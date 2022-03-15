6b9a1b1b6599bddcc976a3cb580e4bfd521b83f6
Yonathan Assous <yonathanassous@gmail.com>
2022-03-15 09:09:04 +0100

    backup.sh

--- backup.sh
+++ backup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-mysqldump -u optieyesproxi548 -p P6O1L238z9i6wYq optieyesproxi5489ytg85 > backup.sql;
+mysqldump -uoptieyesproxi548 -pP6O1L238z9i6wYq optieyesproxi5489ytg85 > /var/www/vhosts/optieyescommande.com/httpdocs_new/backup.sql;
+bzip2 /var/www/vhosts/optieyescommande.com/httpdocs_new/backup.sql
+echo "MESSAGE TEXT" | mailx -s "Sujet" -a "/var/www/vhosts/optieyescommande.com/httpdocs_new/backup.sql.bz2" yonathan.optieyes@gmail.com
+#echo "Finished"

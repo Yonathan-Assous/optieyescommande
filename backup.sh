04e939db69cfc16b633ef17b6d57050e380d7ea3
Yonathan Assous <yonathanassous@gmail.com>
2022-03-16 11:27:27 +0100

    pdf + gitignore

--- backup.sh
+++ backup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-mysqldump -uoptieyesproxi548 -pP6O1L238z9i6wYq optieyesproxi5489ytg85 > /var/www/vhosts/optieyescommande.com/httpdocs_new/backup.sql;
-bzip2 /var/www/vhosts/optieyescommande.com/httpdocs_new/backup.sql
-echo "MESSAGE TEXT" | mailx -s "Sujet" -a "/var/www/vhosts/optieyescommande.com/httpdocs_new/backup.sql.bz2" yonathan.optieyes@gmail.com
-#echo "Finished"
+DAYOFWEEK=$(date +%u);
+mysqldump -uoptieyesproxi548 -pP6O1L238z9i6wYq optieyesproxi5489ytg85 > /var/www/vhosts/optieyescommande.com/httpdocs_new/backup-$DAYOFWEEK.sql;
+rm -r /var/www/vhosts/optieyescommande.com/httpdocs_new/backup-$DAYOFWEEK.sql.bz2
+bzip2 /var/www/vhosts/optieyescommande.com/httpdocs_new/backup-$DAYOFWEEK.sql
+/root/.google-drive-upload/bin/gupload /var/www/vhosts/optieyescommande.com/httpdocs_new/backup-$DAYOFWEEK.sql.bz2
+echo "SQL reçu dans le Drive" | mailx -s "Fichier sql bien reçu dans le drive" yonathan.optieyes@gmail.com
+
+
